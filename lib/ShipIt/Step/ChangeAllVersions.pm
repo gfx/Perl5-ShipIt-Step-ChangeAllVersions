3bc76e9cb49054983b0e9e98b337e3c44aef9e9e
Fuji, Goro <fuji.goro@dena.jp>
2011-12-01 10:38:27 +0900

    Checking in changes prior to tagging of version 0.007.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 69d6e13..77c24f5 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension ShipIt::Step::ChangeAllVersions
    
    +0.007 2011-12-01 10:31:58
    +    - Ignore symbolic links
    +
     0.006 Wed Mar 31 10:59:45 2010
         - Allow the 'v' prefix in VERSION sections

--- lib/ShipIt/Step/ChangeAllVersions.pm
+++ lib/ShipIt/Step/ChangeAllVersions.pm
@@ -4,7 +4,7 @@ use 5.008_001;
 use strict;
 use warnings;
 
-our $VERSION = '0.006';
+our $VERSION = '0.007';
 
 use parent qw(ShipIt::Step);
 use Fatal qw(open close rename opendir closedir);
@@ -130,7 +130,7 @@ ShipIt::Step::ChangeAllVersions - Changes version information in all the modules
 
 =head1 VERSION
 
-This document describes ShipIt::Step::ChangeAllVersions version 0.006.
+This document describes ShipIt::Step::ChangeAllVersions version 0.007.
 
 =head1 SYNOPSIS
 
